241976feacda837b5f419183bb1daf0abdb5f4b8
mcn <695627887@qq.com>
2026-05-23 09:29:41 +0800

    添加项目文件。

---
+++ MauiMultimedia.Shell/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiMultimedia.Shell
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
